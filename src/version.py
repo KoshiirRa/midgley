37a7d140c947c37bb86c401ab7c89aeeba6bc129
KoshiirRa <m.cubed.3@gmail.com>
2026-09-17 14:07:05 -0500

    chore(release): bump package version to v0.6.0

--- src/version.py
+++ src/version.py
@@ -13,7 +13,7 @@ from typing import Optional
 
 logger = logging.getLogger(__name__)
 
-FALLBACK_PACKAGE_VERSION = "0.5.6"
+FALLBACK_PACKAGE_VERSION = "0.6.0"
 FALLBACK_MODEL_VERSION = "v1.6 Ipatieff"
 
 
